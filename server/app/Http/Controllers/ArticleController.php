dca42c0e9f67412926f19278b9995d2dd180f323
Hugo Hespelle <hugohespelle@Hugos-MacBook-Air.local>
2023-03-08 09:17:02 +0100

    controller add

--- server/app/Http/Controllers/ArticleController.php
+++ server/app/Http/Controllers/ArticleController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function add(){
-        echo "ok";
+    public function add(Request $request){
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'content' => 'required|string|max:255',
+            'thumbnailURL' => 'required|string|max:255',
+            'mediaType' => 'string|max:255',
+            'mediaURL' => 'string|max:255',
+            'leadStory' => 'required|integer|max:1'
+        ]); 
+        
+        $article = Article::create([
+            'title' => $request->input('title'),
+            'content' => $request->input('content'),
+            'thumbnailURL' => $request->input('thumbnailURL'),
+            'mediaType' => $request->input('mediaType'),
+            'mediaURL' => $request->input('mediaURL'),
+            'leadStory' => $request->input('leadStory'),
+        ]);
+
+        return $article;
+
     }
 }
